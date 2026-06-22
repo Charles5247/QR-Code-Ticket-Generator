dc4287acc56cdd3c7bba3b469427d8d5a6cdb72c
unknown <cjemeka2015@gmail.com>
2026-06-22 16:21:51 +0100

    React imports added

--- src/boot.js
+++ src/boot.js
@@ -1,11 +1,13 @@
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { createClient } from "@supabase/supabase-js";
 import Chart from "chart.js/auto";
 import { Html5Qrcode } from "html5-qrcode";
 import jsPDF from "jspdf";
 import QRCode from "qrcode";
 
-
+window.React = React;
+window.ReactDOM = ReactDOM;
 window.supabase = { createClient };
 window.Chart = Chart;
 window.Html5Qrcode = Html5Qrcode;
